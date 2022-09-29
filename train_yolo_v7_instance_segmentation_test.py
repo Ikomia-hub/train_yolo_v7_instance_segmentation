8a25834a356c430383ebb90e19728196578d5b9f
ambroise <ambroise.berthe@ikomia.com>
2022-09-29 17:06:13 +0200

    add test file

---
+++ train_yolo_v7_instance_segmentation_test.py
@@ -0,0 +1,17 @@
+import logging
+from ikomia.core import task
+from ikomia.utils.tests import run_for_test
+
+logger = logging.getLogger(__name__)
+
+
+def test(t, data_dict):
+    logger.info("===== Test::train yolo v7 instance segmentation =====")
+    input_dataset = t.getInput(0)
+    params = task.get_parameters(t)
+    params["epochs"] = 2
+    params["batch_size"] = 1
+    params["dataset_split_ratio"] = 0.5
+    task.set_parameters(t, params)
+    input_dataset.load(data_dict["datasets"]["instance_segmentation"]["dataset_coco"])
+    yield run_for_test(t)
