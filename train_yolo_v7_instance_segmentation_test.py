83127bea19d473165a29805496cc0eabdd1b88e2
ambroise <ambroise.berthe@ikomia.com>
2023-08-03 17:24:55 +0200

    Fix test file

--- train_yolo_v7_instance_segmentation_test.py
+++ train_yolo_v7_instance_segmentation_test.py
@@ -8,10 +8,10 @@ logger = logging.getLogger(__name__)
 def test(t, data_dict):
     logger.info("===== Test::train yolo v7 instance segmentation =====")
     input_dataset = t.get_input(0)
-    params = task.get_parameters(t)
+    params = t.get_param_object()
     params["epochs"] = 2
     params["batch_size"] = 1
     params["dataset_split_ratio"] = 0.5
-    task.set_parameters(t, params)
+    t.set_parameters(params)
     input_dataset.load(data_dict["datasets"]["instance_segmentation"]["dataset_coco"])
     yield run_for_test(t)
